c07d99f6b908f6bb938a7f3f56c708d3ad922ca8
James Shaw <js102@zepler.net>
2017-01-08 11:39:05 +0000

    user bind-mount volume, add clean script

--- docker-run.local.sh
+++ docker-run.local.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 set -e
+source docker.config
+echo $BUILD_DIR
+
+docker volume create --opt type=none --opt device=$BUILD_DIR --opt o=bind thesis-build
 docker run -it -v /home/jshaw/thesis:/home/phd/src/thesis -v thesis-build:/home/phd/build hertzsprung/thesis $@
