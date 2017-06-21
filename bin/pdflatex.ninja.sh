7d84322e0653813eb1e344cc5a6817a70769dc8a
James Shaw <js102@zepler.net>
2017-06-21 15:45:24 +0100

    configure bibtex

---
+++ bin/pdflatex.ninja.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+display_usage() {
+	echo -e "Usage: pdflatex.ninja.sh <in> <out>\n"
+}
+
+if [  $# -le 1 ]
+then
+	display_usage
+	exit 1
+fi
+
+srcdir=$(dirname $1)
+builddir=$(dirname $2)
+document=$(basename $1 .tex)
+pdflatex="pdflatex -interaction=batchmode -halt-on-error -output-directory=$builddir"
+
+$pdflatex -draft $1
+bibtex $builddir/$document
+$pdflatex -draft $1
+$pdflatex $1
--- docker-run.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-set -e
-source docker.config
-
-docker volume create --opt type=none --opt device=$BUILD_DIR --opt o=bind thesis-build
-docker run -it -v thesis-build:/home/phd/build hertzsprung/thesis $@
