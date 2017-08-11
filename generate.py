653141a13f4f81e39f711fcf4e84176bd09acf97
James Shaw <js102@zepler.net>
2017-08-11 13:38:17 +0100

    move vonNeumann appendix into body

--- generate.py
+++ generate.py
@@ -46,7 +46,6 @@ class Thesis:
                         'src/thesis/cubicFit/interior-stencils.tex',
                         'src/thesis/cubicFit/double-upwind-stencil.tex',
                         'src/thesis/cubicFit/boundary-stencils.tex',
-                        'src/thesis/cubicFit/vonNeumann.tex',
                         'src/thesis/cubicFit/spherical.tex',
                         'src/thesis/slanted.tex',
                         'src/thesis/slanted/mountainAdvect.tex',
