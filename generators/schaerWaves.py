394f7844e6fefdaebc9667a5fda60d97d7e410e1
James Shaw <js102@zepler.net>
2017-09-07 17:32:26 +0100

    add schaerWaves-btf-300dz-linearUpwind w contour plot

--- generators/schaerWaves.py
+++ generators/schaerWaves.py
@@ -1,9 +1,27 @@
+from ninjaopenfoam import Case, GmtPlot, GmtPlotCopyCase
+
 class SchaerWaves:
     def __init__(self):
-        pass
+        self.btf300dzLinearUpwind = GmtPlotCopyCase(
+                'schaerWaves-btf-300dz-linearUpwind',
+                source='$atmostests_builddir',
+                target='$builddir',
+                plots=['src/thesis/slanted/schaerWaves/w.gmtdict'],
+                files=['18000/Uf'])
+
+        btf300dzLinearUpwindCase = Case('schaerWaves-btf-300dz-linearUpwind')
+
+        self.btf300dzLinearUpwindW = GmtPlot(
+            'schaerWaves-btf-300dz-linearUpwind-w',
+            plot='w',
+            case=btf300dzLinearUpwindCase,
+            time=18000,
+            data=['18000/Uf'])
 
     def outputs(self):
-        return ['src/thesis/slanted/schaerWaves/melvin2010-w-mass-conserving-sisl.png']
+        return ['src/thesis/slanted/schaerWaves/melvin2010-w-mass-conserving-sisl.png'] \
+             + self.btf300dzLinearUpwindW.outputs()
 
     def addTo(self, build):
-        pass
+        build.add(self.btf300dzLinearUpwind)
+        build.add(self.btf300dzLinearUpwindW)
