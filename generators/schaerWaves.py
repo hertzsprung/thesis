29adff11e9dc199df9b85b86445bda1746bdd964
James Shaw <js102@zepler.net>
2017-09-18 15:02:46 +0100

    plot schaerWaves thetaDiff

--- generators/schaerWaves.py
+++ generators/schaerWaves.py
@@ -4,6 +4,10 @@ import os
 
 class SchaerWaves:
     def __init__(self):
+        self.linearUpwindW()
+        self.thetaDiff()
+
+    def linearUpwindW(self):
         self.btf300dzLinearUpwind = GmtPlotCopyCase(
                 'schaerWaves-btf-300dz-linearUpwind',
                 source='$atmostests_builddir',
@@ -23,15 +27,46 @@ class SchaerWaves:
         self.btf300dzLinearUpwindWFigure = PDFLaTeXFigure(
                 'schaerWaves-btf-300dz-linearUpwind-w-figure',
                 output=os.path.join('thesis/slanted/schaerWaves/fig-btf-300dz-linearUpwind-w'),
-                figure=os.path.join('src/thesis/slanted/mountainAdvect/fig-btf-300dz-linearUpwind-w'),
+                figure=os.path.join('src/thesis/slanted/schaerWaves/fig-btf-300dz-linearUpwind-w'),
                 components=self.btf300dzLinearUpwindW.outputs()
+        )
+
+    def thetaDiff(self):
+        self.btf50dzCubicFit = GmtPlotCopyCase(
+                'schaerWaves-btf-50dz-cubicFit',
+                source='$atmostests_builddir',
+                target='$builddir',
+                plots=['src/thesis/slanted/schaerWaves/thetaDiff.gmtdict'],
+                files=['18000/theta_diff'])
+
+        btf50dzCubicFitCase = Case('schaerWaves-btf-50dz-cubicFit')
+
+        self.btf50dzCubicFitThetaDiff = GmtPlot(
+            'schaerWaves-btf-50dz-cubicFit-thetaDiff',
+            plot='thetaDiff',
+            case=btf50dzCubicFitCase,
+            time=18000,
+            data=['18000/theta_diff'],
+            colorBar='legends/thetaDiff_theta_diff.eps')
+
+        self.btf50dzCubicFitThetaDiffFigure = PDFLaTeXFigure(
+                'schaerWaves-btf-50dz-cubicFit-thetaDiff-figure',
+                output=os.path.join('thesis/slanted/schaerWaves/fig-btf-50dz-cubicFit-thetaDiff'),
+                figure=os.path.join('src/thesis/slanted/schaerWaves/fig-btf-50dz-cubicFit-thetaDiff'),
+                components=self.btf50dzCubicFitThetaDiff.outputs()
         )
 
     def outputs(self):
         return ['src/thesis/slanted/schaerWaves/melvin2010-w-mass-conserving-sisl.png'] \
-             + self.btf300dzLinearUpwindWFigure.outputs()
+             + self.btf300dzLinearUpwindWFigure.outputs() \
+             + self.btf50dzCubicFitThetaDiffFigure.outputs()
 
     def addTo(self, build):
         build.add(self.btf300dzLinearUpwind)
+        build.add(self.btf50dzCubicFit)
+
         build.add(self.btf300dzLinearUpwindW)
         build.add(self.btf300dzLinearUpwindWFigure)
+
+        build.add(self.btf50dzCubicFitThetaDiff)
+        build.add(self.btf50dzCubicFitThetaDiffFigure)
