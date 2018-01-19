298101bac6c64274e14ff95c2f8234932ae4ba3e
James Shaw <js102@zepler.net>
2018-01-19 16:31:34 +0000

    plot more arakawaKonor results

--- generators/arakawaKonor.py
+++ generators/arakawaKonor.py
@@ -1,32 +1,19 @@
-from ninjaopenfoam import Case, GmtPlot, GmtPlotCopyCase
+from ninjaopenfoam import Case, GmtPlot, GmtPlotCopyCase, PDFLaTeXFigure
 
 import os
 
 class ArakawaKonor:
     def __init__(self):
-        self.uniformLorenz = GmtPlotCopyCase(
-                'arakawaKonor-uniform-lorenz',
-                source='$atmostests_builddir',
-                target='$builddir',
-                plots=[
-                    'src/thesis/cp/arakawaKonor/theta_diff.gmtdict',
-                    'src/thesis/cp/arakawaKonor/theta_diffS.gmtdict'
-                ],
-                files=['0/theta_diff', '172800/theta_diff'])
-
-        self.uniformCP = GmtPlotCopyCase(
-                'arakawaKonor-uniform-cp',
-                source='$atmostests_builddir',
-                target='$builddir',
-                plots=['src/thesis/cp/arakawaKonor/theta_diffS.gmtdict'],
-                files=['172800/theta_diff'])
+        self.copyCases()
 
         uniformLorenzCase = Case('arakawaKonor-uniform-lorenz')
         uniformCPCase = Case('arakawaKonor-uniform-cp')
+        horizontalGradingLorenzCase = Case('arakawaKonor-horizontalGrading-lorenz')
+        horizontalGradingCPCase = Case('arakawaKonor-horizontalGrading-cp')
 
-        self.initialThetaDiff = GmtPlot(
-            'arakawaKonor-initial-theta_diff',
-            plot='theta_diff',
+        self.uniformInitialThetaDiff = GmtPlot(
+            'arakawaKonor-uniform-initial-theta_diff',
+            plot='theta_diffW',
             case=uniformLorenzCase,
             time=0,
             data=['0/theta_diff'],
@@ -34,26 +21,136 @@ class ArakawaKonor:
 
         self.uniformLorenzThetaDiff = GmtPlot(
             'arakawaKonor-uniform-lorenz-theta_diff',
-            plot='theta_diffS',
+            plot='theta_diffSW',
             case=uniformLorenzCase,
             time=172800,
             data=['172800/theta_diff'])
 
         self.uniformCPThetaDiff = GmtPlot(
             'arakawaKonor-uniform-cp-theta_diff',
-            plot='theta_diffS',
+            plot='theta_diffSW',
             case=uniformCPCase,
             time=172800,
             data=['172800/theta_diff'])
 
+        self.horizontalGradingInitialThetaDiff = GmtPlot(
+            'arakawaKonor-horizontalGrading-initial-theta_diff',
+            plot='theta_diff',
+            case=horizontalGradingLorenzCase,
+            time=0,
+            data=['0/theta_diff'])
+
+        self.horizontalGradingLorenzThetaDiff = GmtPlot(
+            'arakawaKonor-horizontalGrading-lorenz-theta_diff',
+            plot='theta_diffS',
+            case=horizontalGradingLorenzCase,
+            time=172800,
+            data=['172800/theta_diff'])
+
+        self.horizontalGradingCPThetaDiff = GmtPlot(
+            'arakawaKonor-horizontalGrading-cp-theta_diff',
+            plot='theta_diffS',
+            case=horizontalGradingCPCase,
+            time=172800,
+            data=['172800/theta_diff'])
+        
+        self.thetaDiffFigure = PDFLaTeXFigure(
+                'arakawaKonor-fig-theta_diff',
+                output=os.path.join('thesis/cp/arakawaKonor/fig-theta_diff'),
+                figure=os.path.join('src/thesis/cp/arakawaKonor/fig-theta_diff'),
+                components=self.uniformInitialThetaDiff.outputs() + \
+                           self.uniformLorenzThetaDiff.outputs() + \
+                           self.uniformCPThetaDiff.outputs() + \
+                           self.horizontalGradingInitialThetaDiff.outputs() + \
+                           self.horizontalGradingLorenzThetaDiff.outputs() + \
+                           self.horizontalGradingCPThetaDiff.outputs()
+        )
+
+        self.meshes()
+        self.meshesFigure = PDFLaTeXFigure(
+                'arakawaKonor-fig-meshes',
+                output=os.path.join('thesis/cp/arakawaKonor/fig-meshes'),
+                figure=os.path.join('src/thesis/cp/arakawaKonor/fig-meshes'),
+                components=self.horizontalGradedMesh.outputs() + \
+                           self.verticalGradedMesh.outputs()
+        )
+
+    def copyCases(self):
+        self.uniformLorenz = GmtPlotCopyCase(
+                'arakawaKonor-uniform-lorenz',
+                source='$atmostests_builddir',
+                target='$builddir',
+                plots=[
+                    'src/thesis/cp/arakawaKonor/theta_diffW.gmtdict',
+                    'src/thesis/cp/arakawaKonor/theta_diffSW.gmtdict'
+                ],
+                files=['0/theta_diff', '172800/theta_diff'])
+
+        self.uniformCP = GmtPlotCopyCase(
+                'arakawaKonor-uniform-cp',
+                source='$atmostests_builddir',
+                target='$builddir',
+                plots=['src/thesis/cp/arakawaKonor/theta_diffSW.gmtdict'],
+                files=['172800/theta_diff'])
+
+        self.horizontalGradingLorenz = GmtPlotCopyCase(
+                'arakawaKonor-horizontalGrading-lorenz',
+                source='$atmostests_builddir',
+                target='$builddir',
+                plots=[
+                    'src/thesis/cp/arakawaKonor/meshW.gmtdict',
+                    'src/thesis/cp/arakawaKonor/theta_diff.gmtdict',
+                    'src/thesis/cp/arakawaKonor/theta_diffS.gmtdict'
+                ],
+                files=['0/theta_diff', '172800/theta_diff'])
+
+        self.horizontalGradingCP = GmtPlotCopyCase(
+                'arakawaKonor-horizontalGrading-cp',
+                source='$atmostests_builddir',
+                target='$builddir',
+                plots=['src/thesis/cp/arakawaKonor/theta_diffS.gmtdict'],
+                files=['172800/theta_diff'])
+
+        self.verticalGradingLorenz = GmtPlotCopyCase(
+                'arakawaKonor-verticalGrading-lorenz',
+                source='$atmostests_builddir',
+                target='$builddir',
+                plots=['src/thesis/cp/arakawaKonor/mesh.gmtdict'])
+
+    def meshes(self):
+        self.horizontalGradedMesh = GmtPlot(
+            'arakawaKonor-horizontalGradedMesh',
+            plot='meshW',
+            case=Case('arakawaKonor-horizontalGrading-lorenz'),
+            time='constant'
+        )
+
+        self.verticalGradedMesh = GmtPlot(
+            'arakawaKonor-verticalGradedMesh',
+            plot='mesh',
+            case=Case('arakawaKonor-verticalGrading-lorenz'),
+            time='constant'
+        )
+
     def outputs(self):
-        return self.initialThetaDiff.outputs() + \
-               self.uniformLorenzThetaDiff.outputs() + \
-               self.uniformCPThetaDiff.outputs()
+        return self.thetaDiffFigure.outputs() + \
+               self.meshesFigure.outputs()
 
     def addTo(self, build):
         build.add(self.uniformLorenz)
         build.add(self.uniformCP)
-        build.add(self.initialThetaDiff)
+        build.add(self.horizontalGradingLorenz)
+        build.add(self.horizontalGradingCP)
+        build.add(self.verticalGradingLorenz)
+
+        build.add(self.uniformInitialThetaDiff)
         build.add(self.uniformLorenzThetaDiff)
         build.add(self.uniformCPThetaDiff)
+        build.add(self.horizontalGradingInitialThetaDiff)
+        build.add(self.horizontalGradingLorenzThetaDiff)
+        build.add(self.horizontalGradingCPThetaDiff)
+        build.add(self.thetaDiffFigure)
+
+        build.add(self.horizontalGradedMesh)
+        build.add(self.verticalGradedMesh)
+        build.add(self.meshesFigure)
